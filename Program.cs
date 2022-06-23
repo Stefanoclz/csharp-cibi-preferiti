b920543afb54df75d3d0fc9111bfdc49515bf1b4
Stefano Calzoni <stefanoclz@hotmail.it>
2022-06-23 16:22:17 +0200

    Aggiunta richiesta cibo all'utente e stampa cibo preferito con controllo quello randomico del pc, manca controllo input

--- Program.cs
+++ Program.cs
@@ -6,9 +6,30 @@ string[] cibiPreferiti = { "Carbonara", "Hamburger", "Pizza", "Meringata", "Bist
 
 Console.WriteLine("La lista dei cibi contiene " + cibiPreferiti.Length + " elementi");
 
-for(int i = 0; i < cibiPreferiti.Length; i++)
+string answer = Console.ReadLine();
+
+int random = new Random().Next(0, 6);
+
+
+for (int i = 0; i < cibiPreferiti.Length; i++)
 {
-    Console.WriteLine((i+1) + " " + cibiPreferiti[i]);
+    if (answer == cibiPreferiti[i] && random == i)
+    {
+        Console.WriteLine((i + 1) + " " + cibiPreferiti[i] + " --> Stessi gusti bro!");
+    }
+    else if(random == i)
+    {
+        Console.WriteLine((i + 1) + " " + cibiPreferiti[i] + " --> Cibo preferito dal PC");
+    } 
+    else if (answer == cibiPreferiti[i])
+    {
+        Console.WriteLine((i + 1) + " " + cibiPreferiti[i] + " --> Cibo preferito dall'utente");
+    }
+    else
+    {
+        Console.WriteLine((i + 1) + " " + cibiPreferiti[i]);
+    }
+    
 }
 
 Console.WriteLine("Il mio cibo preferito è: " + cibiPreferiti[0]);
@@ -25,3 +46,4 @@ if(cibiPreferiti.Length % 2 == 0)
 {
     Console.WriteLine("Il cibo al centro è: " + cibiPreferiti[cibiPreferiti.Length / 2]);
 }
+
