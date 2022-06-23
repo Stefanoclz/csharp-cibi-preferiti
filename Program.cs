463abe3ebe2ce0166a33cb824bfc5c570860b68a
Stefano Calzoni <stefanoclz@hotmail.it>
2022-06-23 14:52:21 +0200

    Aggiunta stampa cibo preferito

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string[] cibiPreferiti = { "Carbonara", "Hamburger", "Pizza", "Meringata", "Bistecca", "Sushi" };
+
+
+Console.WriteLine("La lista dei cibi contiene " + cibiPreferiti.Length + " elementi");
+
+for(int i = 0; i < cibiPreferiti.Length; i++)
+{
+    Console.WriteLine((i+1) + " " + cibiPreferiti[i]);
+}
+
+Console.WriteLine("Il mio cibo preferito è: "cibiPreferiti[0]);
